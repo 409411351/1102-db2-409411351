43d63f4f10da03d6e7e128dad1759039e8ae8a9b
Darkking789 <henry112pmm4@gmail.com>
2022-05-17 16:51:16 +0800

    w13-P1: create controllers for crown2 for MVS architecture

--- routes/crown2_51.js
+++ routes/crown2_51.js
@@ -4,42 +4,19 @@ var router = express.Router();
 const Category_51 = require('../models/Category_51');
 const shop_51 = require('../models/shop_51');
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  try{
-    let results = await Category_51.fetchAll();
-    console.log('results', JSON.stringify(results));
-    res.render('crown2_51/index', 
-    { 
-      data: results,
-      id: '409411351', 
-      title: 'Crown2_51_DB' 
-    });
-  }catch(err){
-    console.log(err)
-  }
-});
+const crown2Controller_51 = require('../controllers/crown2Controller_51');
 
-router.get('/shop_51/:category', async function(req,res){
-  console.log('category', req.params.category);
-  
+//CREATE
+// router.post('/product_51',)
 
-  try{
-    const cid = await Category_51.fetchCatIdByName(req.params.category);
-    console.log('cid',cid);
-    let results = await shop_51.fetchProductsByCategory(cid);
-    console.log('results',JSON.stringify(results));
+//READ
+router.get('/',crown2Controller_51.getCategories);
 
-    res.render('crown2_51/products_51', 
-    { 
-      data: results,
-      title: req.params.category,
-      id: '409411351', 
-      name: '吳信篁' 
-    });
-  }catch(err){
-    console.log(err);
-  }
-})
+router.get('/shop_51/:category',crown2Controller_51.getProductsByCategory);
+
 
 module.exports = router;
+
+//UPDATE
+
+//DELETE
