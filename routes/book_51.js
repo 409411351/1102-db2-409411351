b4a8f641c0a9efb48d2a5e595cf9c57760cc5fbb
Darkking789 <henry112pmm4@gmail.com>
2022-06-07 17:01:23 +0800

    w16-p3: /book_51/create to book_51/add_51.ejs

--- routes/book_51.js
+++ routes/book_51.js
@@ -3,22 +3,34 @@ var router = express.Router();
 
 const db = require('../utils/database');
 
-/* GET home page. */
+/* READ */
 router.get('/',async (req, res) => {
     try{
         const results = await db.query(`SELECT * FROM book_51`);
         console.log('results',JSON.stringify(results.rows)); 
 
-        res.render('book_51/index', { 
+        res.render('book_51', { 
             id: '409411351', 
             name: '吳信篁' ,
             data:results.rows,
         });
 
     }catch(error){
+        res.render('book_51', { 
+            id: '409411351', 
+            name: '吳信篁' ,
+            data: '',
+        });
         console.log('err',error);
     }
-    
+});
+
+router.get('/create',(req,res) => {
+    res.render('book_51/add_51',
+    {
+        id:'409411351',
+        name:'吳信篁',
+    });
 });
 
 module.exports = router;
