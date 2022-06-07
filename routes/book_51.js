6d3e7ed39b79ee15111879a456ce0fc3a588754d
Darkking789 <henry112pmm4@gmail.com>
2022-06-07 17:45:54 +0800

    w16-p4: POST /book_51/create to create a book data

--- routes/book_51.js
+++ routes/book_51.js
@@ -3,6 +3,26 @@ var router = express.Router();
 
 const db = require('../utils/database');
 
+// CREATE
+router.post('/create',async (req,res) => {
+    // console.log('body',req.body);
+    const id = req.body.id;
+    const name = req.body.name;
+    const author = req.body.author;
+    const price = req.body.price;
+
+    try{
+        const query = {
+            text: `INSERT INTO book_51(id,name,author,price) VALUES ($1, $2, $3, $4)`,
+            values:[id, name, author, price],
+        };
+        await db.query(query);
+        res.redirect('/book_51');
+    }catch(error){
+        console.log(error);
+    }
+});
+
 /* READ */
 router.get('/',async (req, res) => {
     try{
